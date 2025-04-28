0e7cd5b3a8cd18f94a45f3f8b6bc0804e08d35b5
Eduardo Villota <eduardouio7@gmail.com>
2025-04-28 09:35:25 -0500

    Actualizamos el origen

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,10 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 CSRF_TRUSTED_ORIGINS = [
+    'http://146.190.56.181',
+    'http://146.190.56.181:8000',
+    'https://146.190.56.181',
     'https://146.190.56.181:8000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
-
-
 
 # Application definition
 
