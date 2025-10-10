12e6ca7ae194fb64e42dcf0f8063e5a62f97f552
Eduardo Villota <eduardouio7@gmail.com>
2025-10-10 06:03:29 -0500

    feat: Reemplazar la configuración de la base de datos por la importación de DATABASE_DEFAULT

--- app/backend/src/config/settings.py
+++ app/backend/src/config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from .secrets import DATABASE_DEFAULT
 
 
 # Quick-start development settings - unsuitable for production
@@ -87,12 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASE_DEFAULT
 
 
 # Password validation
