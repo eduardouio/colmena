76bca183a0f3683db9ab3aa4d9f5f9da36d16e96
eduardo <eduardouio7@gmail.com>
2025-11-04 16:05:18 -0500

    listado de jugadores

--- app/backend/src/clubs/urls.py
+++ app/backend/src/clubs/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views.PlayersClubLists import PlayersClubListView
 
-app_name = 'clubs'  # Este es el namespace que debe estar definido
+app_name = 'clubs'
 
 urlpatterns = [
-    # Agregar aquí otras URLs existentes del módulo clubs si las hay
     path('club/<int:club_id>/players/', PlayersClubListView.as_view(), name='players_list'),
 ]
